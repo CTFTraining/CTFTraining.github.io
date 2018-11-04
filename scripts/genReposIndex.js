6c652edec13d0a99365a679c672148a644ed9651
Virink <virink@outlook.com>
2018-11-04 17:14:52 +0800

    fix
    Signed-off-by: Virink <virink@outlook.com>

--- scripts/genReposIndex.js
+++ scripts/genReposIndex.js
@@ -25,7 +25,7 @@ function unique(arr) {
 	var hash = [];
 	for (var i = 0; i < arr.length; i++) {
 		for (var j = i + 1; j < arr.length; j++) {
-			if (arr[i] === arr[j]) {
+			if (arr[i]['name'] === arr[j]['name']) {
 				++i;
 			}
 		}
